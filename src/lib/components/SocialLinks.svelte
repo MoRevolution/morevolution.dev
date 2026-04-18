357cef5a180a790eaf441a8bc17ac525aa5fc981
Motti Kelbessa <81017119+MoRevolution@users.noreply.github.com>
2026-04-17 21:29:58 -0500

    switching to cal.com
    Signed-off-by: Motti Kelbessa <81017119+MoRevolution@users.noreply.github.com>

--- src/lib/components/SocialLinks.svelte
+++ src/lib/components/SocialLinks.svelte
@@ -11,7 +11,7 @@
 		<svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
 		email
 	</a>
-	<a href="https://calendly.com/morevolution" target="_blank" rel="noopener noreferrer">
+	<a href="https://cal.com/morevolution" target="_blank" rel="noopener noreferrer">
 		<svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2z"/></svg>
 		meet
 	</a>
